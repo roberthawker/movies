1a05058362f7ece7f4fe6a0600acf46a60b50c1b
Robert Hawker <roberthawker16@gmail.com>
2021-02-05 03:45:09 +0000

    First commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "166990df217c1603ff85";
+/******/ 	var hotCurrentHash = "1ef27d2df042e5e8d9ce";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    env: {
-        MONGODB_URI: 'mongodb+srv://admin-robert:Greencat22-@mymovieapi.ipuzd.mongodb.net/cinema?retryWrites=true&w=majority',
-    },
-  }
-
-
-  
